76b2c747b87b0a52b3ed0148a854ff0fdf5937c6
akalke <amaeya.kalke@gmail.com>
2014-10-12 17:50:18 -0500

    Initial Commit

---
+++ LionsAndTigers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers
+//
+//  Created by Amaeya Kalke on 10/12/14.
+//  Copyright (c) 2014 Amaeya Kalke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
